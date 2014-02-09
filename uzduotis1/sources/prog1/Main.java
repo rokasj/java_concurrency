b9c5b5b81325071f020dd2d02ab3c27d724c5228
Rokas Juozapavičius <juozapavicius.rokas@gmail.com>
2014-02-09 18:55:44 +0200

    Create Main.java

---
+++ uzduotis1/sources/prog1/Main.java
@@ -0,0 +1,35 @@
+/*
+* Main.java
+* @author Rokas Juozapavicius
+* 1. Uzduotis. Lyginiu-nelyginiu musis
+*/
+
+package prog1;
+
+/*
+Lyginiu-nelyginiu musis.
+2 tipu gijos i ta pati lauka talpina reiksme.
+Lyginio tipo - lygine.
+Nelyginio tipo - nelygine.
+Laimi gija, kurios talpinama reiksme, pasibaigus programai,
+sutampa su giju keiciamo bendro lauko reiksme.
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       try {
+           if ((args[0] != null) && (args[0].equals("synchronized"))) {
+               EvenOddThread.sync = true;
+           }
+       } catch (ArrayIndexOutOfBoundsException exc) {
+            //System.out.println("Ivyko klaida " + exc);
+       }
+       System.out.println("Programa pradeda darba.");
+       EvenOddThread.pradeti();
+       System.out.println("Programa baigia darba.");
+
+    }
+
+}
