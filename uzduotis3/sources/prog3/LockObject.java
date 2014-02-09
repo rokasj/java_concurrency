9efe2b7ac75dbf4bf6aceff58c81dd199c879799
Rokas Juozapavičius <juozapavicius.rokas@gmail.com>
2014-02-09 19:12:17 +0200

    Create LockObject.java

---
+++ uzduotis3/sources/prog3/LockObject.java
@@ -0,0 +1,15 @@
+/*
+   LockObject.java
+   @Author R. Juozapavicius
+   3. Uzduotis. Apibreztinio integralo skaiciavimas
+
+   Klase - uzraktas (reikalinga giju darbo sinchronizavimui).
+*/
+
+package prog3;
+
+public class LockObject {
+
+    public LockObject () { }
+
+}
