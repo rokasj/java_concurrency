d01c8984f0af24e01458e7cd4d39dafc2a0280f7
Rokas Juozapavičius <juozapavicius.rokas@gmail.com>
2014-02-09 18:56:23 +0200

    Create MutualField.java

---
+++ uzduotis1/sources/prog1/MutualField.java
@@ -0,0 +1,24 @@
+/*
+* MutualField.java
+* @author Rokas Juozapavicius
+* 1. Uzduotis. Lyginiu-nelyginiu musis
+*/
+
+package prog1;
+
+/*
+Bendrai giju naudojamo/-u objekto/-u klase.
+*/
+
+class MutualField {
+    // Laukas, kuri skaitys/modifikuos kelios gijos
+    int n;
+
+    // Metodas modifikuojantis objekto turini
+    public void priskirti(int skaicius) {
+
+        n = skaicius; // Priskyrimas
+
+    }
+
+}
